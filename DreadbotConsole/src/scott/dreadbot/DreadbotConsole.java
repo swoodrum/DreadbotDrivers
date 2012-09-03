5a8f02b0e140d2a40a572e0bff4d7d347dfdc6e6
Scott Woodrum <Scott's Dell i5@MININT-HGRU73E>
2012-09-03 18:02:48 -0500

    adding game pad stick and button handling

--- DreadbotConsole/src/scott/dreadbot/DreadbotConsole.java
+++ DreadbotConsole/src/scott/dreadbot/DreadbotConsole.java
@@ -107,8 +107,98 @@ public class DreadbotConsole {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-
+				controller.poll();
+				int compassDir = controller.getHatDir();
+				if (compassDir != GamePadController.NONE)
+					// getLogger().debug("Compass direction: " + compassDir);
+					switch (compassDir) {
+					case GamePadController.EAST:
+						getLogger().debug("Compass direction: EAST");
+						break;
+					case GamePadController.WEST:
+						getLogger().debug("Compass direction: WEST");
+						break;
+					case GamePadController.NORTH:
+						getLogger().debug("Compass direction: NORTH");
+						break;
+					case GamePadController.SOUTH:
+						getLogger().debug("Compass direction: SOUTH");
+						break;
+					case GamePadController.NW:
+						getLogger().debug("Compass direction: NORTHWEST");
+						break;
+					case GamePadController.NE:
+						getLogger().debug("Compass direction: NORTHEAST");
+						break;
+					case GamePadController.SW:
+						getLogger().debug("Compass direction: SOUTHWEST");
+						break;
+					case GamePadController.SE:
+						getLogger().debug("Compass direction: SOUTHEAST");
+						break;
+					}
+				int xyDir = controller.getXYStickDir();
+				if (xyDir != GamePadController.NONE)
+					switch (xyDir) {
+					case GamePadController.EAST:
+						getLogger().debug("XY direction: EAST");
+						break;
+					case GamePadController.WEST:
+						getLogger().debug("XY direction: WEST");
+						break;
+					case GamePadController.NORTH:
+						getLogger().debug("XY direction: NORTH");
+						break;
+					case GamePadController.SOUTH:
+						getLogger().debug("XY direction: SOUTH");
+						break;
+					case GamePadController.NW:
+						getLogger().debug("XY direction: NORTHWEST");
+						break;
+					case GamePadController.NE:
+						getLogger().debug("XY direction: NORTHEAST");
+						break;
+					case GamePadController.SW:
+						getLogger().debug("XY direction: SOUTHWEST");
+						break;
+					case GamePadController.SE:
+						getLogger().debug("XY direction: SOUTHEAST");
+						break;
+					}
+				int zrzDir = controller.getZRZStickDir();
+				if (zrzDir != GamePadController.NONE)
+					switch (zrzDir) {
+					case GamePadController.EAST:
+						getLogger().debug("ZRZ direction: EAST");
+						break;
+					case GamePadController.WEST:
+						getLogger().debug("ZRZ direction: WEST");
+						break;
+					case GamePadController.NORTH:
+						getLogger().debug("ZRZ direction: NORTH");
+						break;
+					case GamePadController.SOUTH:
+						getLogger().debug("ZRZ direction: SOUTH");
+						break;
+					case GamePadController.NW:
+						getLogger().debug("ZRZ direction: NORTHWEST");
+						break;
+					case GamePadController.NE:
+						getLogger().debug("ZRZ direction: NORTHEAST");
+						break;
+					case GamePadController.SW:
+						getLogger().debug("ZRZ direction: SOUTHWEST");
+						break;
+					case GamePadController.SE:
+						getLogger().debug("ZRZ direction: SOUTHEAST");
+						break;
+					}
+				boolean[] buttons = controller.getButtons();
+				for (int i = 0; i < GamePadController.NUM_BUTTONS; i++) {
+					if (buttons[i] == true) {
+						getLogger().debug("Button " + (i + 1) + " pressed");
+					}
+				}
 			}
 		});
 
